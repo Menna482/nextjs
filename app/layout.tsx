63e692767247324e8aebd707f2bdf580ad1a3971
menna <mennaradwan128@gmail.com>
2025-10-04 11:29:41 +0300

    fix: ignore ESLint blocking during build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Providers from "./Provider";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 
 export const metadata = {
   title: "E-Shop",
